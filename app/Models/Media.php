525871695108a21845842fbb80ccc9c95b20d67b
leo <you@example.com>
2025-03-15 23:46:08 +0700

    first commit

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Media extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'file_name',
+        'mime_type',
+        'path',
+        'disk',
+        'file_hash',
+        'size',
+        'mediable_id',
+        'mediable_type',
+    ];
+
+     public function mediable(): MorphTo
+    {
+        return $this->morphTo();
+    }
 }
